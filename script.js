9f44042881d66bbe39e275ab255b9f60960c18ba
aledic85 <dicuollo85@gmail.com>
2019-03-05 21:54:06 +0100

    comm 2

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
 function removeOpenMenu() {
-  var right = $(".nav-right .dropdown-menu-i");
+
+  var right = $(".dropdown-menu-i");
   right.slideUp();
 }
 
 function slideClick() {
+
   var en = $(".nav-right h4:nth-child(7)");
   var help = $(".nav-right h4:nth-child(5)");
 
   en.click(function(){
 
+    // removeOpenMenu();
     var me = $(this);
-    removeOpenMenu();
     me.siblings(".m-r-i").slideToggle();
   });
 
   help.click(function(){
 
+    // removeOpenMenu();
     var me = $(this);
-    removeOpenMenu();
     me.siblings(".m-r-ii").slideToggle();
-  })
+  });
 }
 
 function slideHover() {
+
   var accounts = $(".m-r-ii > .dropdown-items");
 
   accounts.hover(function() {
@@ -33,7 +36,7 @@ function slideHover() {
     var me = $(this);
     me.children(".dropdown-menu-ii").hide();
 
-  })
+  });
 }
 
 function init() {
